e0a68d97d30f0eab271fa2185650150901a927c2
Chris6cp <63050667+Chris6cp@users.noreply.github.com>
2020-08-07 17:37:45 +0530

    Add files via upload

---
+++ js/but.js
@@ -0,0 +1,58 @@
+function changeDivImage()
+    {
+        var imgPath = new String();
+        imgPath = document.getElementById("body1").style.backgroundImage;
+
+        if(imgPath == "url(1cur.png)" || imgPath == "")
+        {
+            document.getElementById("body1").style.backgroundImage = "url(1cur_a.png)";
+        }
+        else
+        {
+            document.getElementById("body1").style.backgroundImage = "url(1cur_b.png)";
+        }
+    }
+    function changeDiv2Image()
+    {
+        var imgPath = new String();
+        imgPath = document.getElementById("body1").style.backgroundImage;
+
+        if(imgPath == "url(1cur_a.png)" || imgPath == "")
+        {
+            document.getElementById("body1").style.backgroundImage = "url(1cur_b.png)";
+        }
+        else
+        {
+            document.getElementById("body1").style.backgroundImage = "url(1cur_c.png)";
+        }
+    }
+    function changeDiv3Image()
+    {
+        var imgPath = new String();
+        imgPath = document.getElementById("body1").style.backgroundImage;
+
+        if(imgPath == "url(1cur_b.png)" || imgPath == "")
+        {
+            document.getElementById("body1").style.backgroundImage = "url(1cur_c.png)";
+        }
+        else
+        {
+            document.getElementById("body1").style.backgroundImage = "url(1cur_d.png)";
+        }
+    }
+    
+    
+    function changeDiv5Image()
+    {
+        var imgPath = new String();
+        imgPath = document.getElementById("body1").style.backgroundImage;
+
+        if(imgPath == "url(1cur_d.png)" || imgPath == "")
+        {
+            document.getElementById("body1").style.backgroundImage = "url(1cur_e.jpg)";
+        }
+        else
+        {
+            document.getElementById("body1").style.backgroundImage = "url(1cur_e.png)";
+        }
+    }
